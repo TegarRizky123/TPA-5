1d859f7c9e08da5f0a318cd17771ed0c45cc8906
TegarRizky123 <tegarrizky237@gmail.com>
2022-11-12 15:39:20 +0700

    todoS app TPA-5

---
+++ src/components/Todos.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos } from "../redux/reducer";
+import { GoPlus } from "react-icons/go";
+import { motion } from "framer-motion";
+import swal from 'sweetalert';
+
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+  };
+};
+
+const Todos = (props) => {
+  const [todo, setTodo] = useState("");
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const add = () => {
+    if (todo === "") {
+        swal(
+              ("Please Fill In The Todo First")
+          )
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        item: todo,
+        completed: false,
+      });
+      setTodo("");
+    }
+  };
+  
+  //console.log("props from store", props);
+  return (
+    <div className="addTodos">
+      <input
+        type="text"
+        onChange={(e) => handleChange(e)}
+        className="input-todo"
+        value={todo}
+      />
+
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="add-btn"
+        onClick={() => add()}
+      >
+        <GoPlus />
+      </motion.button>
+      <br />
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
